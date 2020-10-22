f7b1860cb66f3a6dbce79efd789f8b79195ffcf2
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2020-10-22 11:56:36 -0300

    text and colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
 <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptate dignissimos inventore ad eligendi earum, ducimus vel nemo vero assumenda ex? Sequi velit aliquid quisquam nesciunt, quibusdam nihil tenetur delectus!</p>
+    <p class="deep-purple lighten-3 red--text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam illum officia atque vel odit, cumque voluptas ullam culpa, corporis commodi ducimus eum voluptates. Soluta sapiente itaque vero optio impedit ut!</p>
+    <h1 class="display-4 text-uppercase">Massive display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+    <p class="caption font-weight-light">This is a caption</p>
 </div>
 </template>
 
